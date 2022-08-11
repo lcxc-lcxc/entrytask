dc2a2333cc0de8417645a0b54d5179eaa9486e38
laoyanjie <laoyanjie@seamoney.com>
2022-08-11 19:11:52 +0800

    add username cache after comment

--- internel/dao/user.go
+++ internel/dao/user.go
@@ -1,8 +1,13 @@
 package dao
 
 import (
+	"context"
+	"entrytask/internel/constant"
+	"entrytask/internel/dao/cache"
 	"entrytask/internel/model"
+	"entrytask/pkg/utils"
 	"gorm.io/gorm"
+	"log"
 	"time"
 )
 
@@ -22,8 +27,33 @@ func (d *Dao) CreateUser(username, password string) (*model.User, error) {
 // SelectUserByUsername
 // return (nil , err) if the record not found
 func (d *Dao) GetUserByName(username string) (*model.User, error) {
-	user := model.User{
-		Username: username,
+
+	return d.GetUserByNameCache(username)
+}
+
+func (d *Dao) GetUserByNameCache(username string) (*model.User, error) {
+
+	loadFunction := func(ctx context.Context, key any) (*model.User, error) {
+		log.Println("get username cache failed , getting data from database ")
+		redisKey, _ := key.(string)
+		username := utils.ConvertRedisKeyToString(redisKey)
+
+		user := model.User{
+			Username: username,
+		}
+		userRes, err := user.SelectUserByUsername(d.engine)
+		if err != nil {
+			return nil, err
+		}
+		return userRes, nil
 	}
-	return user.SelectUserByUsername(d.engine)
+
+	loadableCache := cache.NewLoadableCache[*model.User](loadFunction, d.RedisClient, time.Hour)
+	product, err := loadableCache.Get(context.Background(), utils.ConvertStringToRedisKey(constant.USERNAME, username))
+
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+
 }
